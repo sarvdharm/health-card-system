217aa22cf8f18036bdde80cccbb6a109f72de41a
sarvdharm <sarvdharmkalyansamiti@gmail.com>
2026-02-04 07:04:54 +0530

    Update auth_system.py

--- auth_system.py
+++ auth_system.py
@@ -2,11 +2,11 @@ import streamlit as st
 import database
 
 def login():
-    st.markdown("### 🔐 Multi-Level Coordinator Login")
-    uid = st.text_input("UserID (Mobile Number)")
+    st.subheader("🔐 Staff Login Portal")
+    uid = st.text_input("UserID (Mobile No)")
     pwd = st.text_input("Password", type="password")
     
-    if st.button("Login"):
+    if st.button("Submit Login"):
         if uid == "admin" and pwd == "ngo786":
             st.session_state.update({"logged_in": True, "user_role": "admin", "user_name": "Chief Admin", "user_id": "0"})
             st.rerun()
@@ -14,10 +14,7 @@ def login():
             df = database.get_users()
             user = df[(df['UserID'].astype(str) == uid) & (df['Pass'].astype(str) == pwd)]
             if not user.empty:
-                st.session_state.update({
-                    "logged_in": True, "user_id": uid, 
-                    "user_role": user.iloc[0]['Role'].lower(), "user_name": user.iloc[0]['Name']
-                })
+                st.session_state.update({"logged_in": True, "user_id": uid, "user_role": user.iloc[0]['Role'].lower(), "user_name": user.iloc[0]['Name']})
                 st.rerun()
             else:
                 st.error("Invalid Credentials!")
